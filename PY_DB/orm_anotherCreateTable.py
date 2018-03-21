38a02b8061778f9c43acc51e9a2dc8650d3f8a8d
Bruce Zhou <peichunzhou1006@gmail.com>
2018-03-22 09:49:53 +1100

    Correct Syntax error; Test Mac connect to github

--- PY_DB/orm_anotherCreateTable.py
+++ PY_DB/orm_anotherCreateTable.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Table, MetaData, Column, Integer, String. ForeignKey
+
+from sqlalchemy import Table, MetaData, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import mapper
 
 metadata = MetaData()
 
 user = Table(
-  'user', medata,
+  'user', metadata,
   Column('id', Integer, primary_key = True),
   Column('name', String(50)),
   Column('fullname', String(50)),
